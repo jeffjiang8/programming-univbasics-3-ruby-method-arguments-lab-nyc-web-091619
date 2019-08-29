e6f8d595a00fcf66d1e28e3a6c4e8b04aed97125
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-08-29 19:22:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,6 +9,12 @@ end
 
 introduction("Jeff")
 
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+introduction_with_language("Jeff", "JavaScript")
+
 
 
 
